17eb1278d493550b727a2fe2e9420f9091809c00
ko <haft0308@gmail.com>
2022-03-18 15:03:52 +0900

    add button

---
+++ Day0317/src/Bbi.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Bbi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int num=0;
+//		num++;
+//		System.out.println("알의 개수"+num);
+//		num++;
+//		System.out.println("알의 개수"+num);
+//		num++;
+//		System.out.println("알의 개수"+num);//수
+//		num++;
+//		System.out.println("알의 개수"+num);
+//		num++;
+//		System.out.println("알의 개수"+num);
+//		num++;
+//		System.out.println("알의 개수"+num);//수
+//		num++;
+//		System.out.println("알의 개수"+num);//수
+//		
+//		int i = 0;
+//		for ( i=0; i<7;i++)//i=0, i=1, 
+//		{
+//			num++;//
+//			System.out.println("알의 개수"+num);//수
+//		}
+//		
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("문자 1개를 입력하세요");
+//		//int inputData=sc.nextInt();//65, 97 'a' //97
+//		String str = sc.next();//"ao"
+//		char ch = str.charAt(0);//'a'//97//'b'98
+//		System.out.println((char)(ch-32));//97-32=65<--A//98-32=66<--B
+//		
+		
+		for ( int i = 0; i <2 ; i++)//i=0, 0<2
+		{
+			System.out.println("예\t예"+i);//"예"+0			
+		}
+			
+		
+		
+	}
+
+}
